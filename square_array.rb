9bed676d9eaa8064020d496f8e80de3899258dd3
tatiana.mojica <tatiana.mojica@bustle.com>
2018-09-29 22:00:01 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,4 +1,20 @@
-def square_array(array)
-  square_array.each do |numbers**| 
-  end
+require "pry" 
 
+def square_array(array)
+  new_array = []
+  # new array is to contain the squared numbers from array
+array.each do |number|  
+  # iterating on the array with the each method, variable number is being declared each element in the array one at a time
+  squared = number ** 2
+  # new variable squared equals return value of the equation
+  new_array.push(squared)
+  # output is pushed into a new array
+end 
+# output is the new array, which are the squared numbers 
+new_array
+end 
+
+# def square_array(array)
+#   array.collect do |number| number**2 
+#   end
+# end 
